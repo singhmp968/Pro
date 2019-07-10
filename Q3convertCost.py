e4d0d94800110ffa44dc53f4bb3c071f247723d6
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-10 15:48:30 +0530

    Update Q3convertCost.py

--- Q3convertCost.py
+++ Q3convertCost.py
@@ -1,11 +1,14 @@
 a,b=input().strip().split(" ")
 co=0
-if (len(a)==len(b)):
-    co=0
-elif (a==b):
-        co=0
 len1=abs(len(a)-len(b))
-for i in range(len(a)):
-        if(a[i]!=b[i]):
-                len1=len1+1
+
+if(a==b):
+        print(0)
+else:
+    for i in range(len(a)):
+        if len(b)==0 and a[i] in b:
+            break
+
+            if(a[i]!=b[i]):
+                    len1=len1+1
 print(len1)
